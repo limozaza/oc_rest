0cd2249e55f70ef3f619b321e6d6d5c4814eab96
limozaza <zakboite@gmail.com>
2017-07-27 15:36:41 +0200

    Deserialize with Fos

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -4,9 +4,14 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Article;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-class ArticleController extends Controller
+class ArticleController extends FOSRestController
 {
     /**
      * @Rest\View()
@@ -16,16 +21,33 @@ class ArticleController extends Controller
      *     requirements={"id"="\d+"}
      * )
      */
-    public function showAction(/*Article $article*/)
+    public function showAction(Article $article)
     {
-        $article = new Article();
-        $article->setTitle('Titre article');
-        $article->setContent('Le contenude larticle');
-        /*$em = $this->getDoctrine()->getManager();
-        $em->persist($article);
-        $em->flush();*/
-
-        //dump($article);exit();
         return $article;
+    }
+
+
+    /**
+     * @Rest\View(
+     *  statusCode=201
+     * )
+     * @Rest\Post(
+     *     path="/articles",
+     *     name="app_articles_create"
+     * )
+     * @ParamConverter("article", converter="fos_rest.request_body")
+     */
+    public function createAction(Article $article)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($article);
+        $em->flush();
+        return $this->view(
+            $article,
+            Response::HTTP_CREATED,
+            [
+                'Location' => $this->generateUrl('app_articles_show',['id' => $article->getId()], UrlGeneratorInterface::ABSOLUTE_URL)
+            ]
+        );
     }
 }
